517dff85d29223f7df7fade835705c5dc55696c0
testadaz <nizet.pierre@gmail.com>
2016-04-28 19:26:56 +0200

    various improvements
    * Correction for empty "title" strings
    * Conversion of the Eyeem code for tags in the corresponding tag with
    the replace_tag_number_by_text function
    * Using of function for code readability
    * Cleaner output for log

--- eyeem2koken.php
+++ eyeem2koken.php
@@ -45,28 +45,14 @@ $koken_api_url = $koken_url.'/api.php?/content';
 $eyeem_limit = "200";
 $eyeem_photos_list = 'users/'.$eyeem_username.'/photos?access_token='.$eyeem_token.'&limit='.$eyeem_limit;
 
-
+echo date('Y-m-d\TH:i:sO'); //for log
+echo '\n';                  //for log
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //Requesting picture list from Eyeem
-$curl = curl_init($eyeem_api_url.$eyeem_photos_list);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$curl_response = curl_exec($curl);
-
-if ($curl_response === false) {
-    $info = curl_getinfo($curl);
-    curl_close($curl);
-    die('error occured during curl exec. Additional info: ' . var_export($info));
-}
-curl_close($curl);
-$decoded = json_decode($curl_response);
-if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-    die('error occured: ' . $decoded->response->errormessage);
-}
-echo 'Request 1 Eyeem ok!';
-echo '<br/>';
-
-$lp = $decoded->photos->total;
-
+$photo_list_data = request($eyeem_api_url.$eyeem_photos_list);
+echo 'Request photo list Eyeem ok!'; //for log
+echo '\n';                           //for log
+$lp = $photo_list_data->photos->total;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //Reading last picture sent
@@ -78,66 +64,68 @@ if(file_exists($file)) {
 	fclose($fh);
 } else {
 	$fh = fopen($file,'w');
-	fwrite($fh,$decoded->photos->items[$lp]->id);
+	fwrite($fh,$photo_list_data->photos->items[$lp]->id);
 	fclose($fh);
 }
 
+echo 'last_photo_uploaded : '.$last_photo_uploaded; //for log
+echo '\n';                                          //for log
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //Identifying picture to sent
+//Extraction de liste des photos les plus anciennes en premier (on sort de la boucle des que l'on a trouvé la derniere photo traitée,)   A SUPPR
 for ($i = $eyeem_limit; $i >= 1; $i--) {
-		if ($decoded->photos->items[$i]->id == $last_photo_uploaded) {
+		if ($photo_list_data->photos->items[$i]->id == $last_photo_uploaded) {
 			$j=$i-1;
 		}
 }
-$eyeem_photo = $decoded->photos->items[$j]->id;
+
+
+$eyeem_photo = $photo_list_data->photos->items[$j]->id;
+echo 'photo_uploading : '.$last_photo_uploaded; //for log
+echo '\n';                                      //for log
+ 
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //Requesting picture details
+//requete 2 Eyeem  A SUPPR
 $eyeem_photos_data = 'photos/'.$eyeem_photo.'?access_token='.$eyeem_token.'&detailed=1';
-
-$curl = curl_init($eyeem_api_url.$eyeem_photos_data);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-$curl_response = curl_exec($curl);
-
-if ($curl_response === false) {
-    $info = curl_getinfo($curl);
-    curl_close($curl);
-    die('error occured during Eyeem curl exec. Additioanl info: ' . var_export($info));
-}
-curl_close($curl);
-$decoded = json_decode($curl_response);
-if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
-    die('Eyeem error occured: ' . $decoded->response->errormessage);
-}
-
+$photo_data = request($eyeem_api_url.$eyeem_photos_data);
+echo 'Request photo details Eyeem ok!'; //for log
+echo '\n';                              //for log
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //Extracting datas 
-$width = $decoded->photo->width; 
-$height = $decoded->photo->height; 
-$latitude = $decoded->photo->latitude; 
-$longitude = $decoded->photo->longitude; 
-$updated = $decoded->photo->updated; 
-$title = $decoded->photo->title; 
-$caption = $decoded->photo->caption; 
-$photoUrl= $decoded->photo->photoUrl; 
+$width = $photo_data->photo->width; 
+$height = $photo_data->photo->height; 
+$latitude = $photo_data->photo->latitude; 
+$longitude = $photo_data->photo->longitude; 
+$updated = $photo_data->photo->updated; 
+$title = replace_tag_number_by_text($photo_data->photo->title); 
+$caption = replace_tag_number_by_text($photo_data->photo->caption); 
+$photoUrl= $photo_data->photo->photoUrl; 
 $url_photo_big = preg_replace('(\d+/\d+)',$width.'/'.$height,$photoUrl,1); 
 $tags = 'mobile_photography,eyeem,'.preg_replace('/\s/',',',$caption);
 
+echo $title; //for log
+echo '\n';   //for log
+if ($title == "") {
+	$title = "Untitled_".$updated;
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // Download locally the picture (with a clean filename)
 $tmp_filename = preg_replace(array('/\s/', '/\.[\.]+/', '/[^\w_\.\-]/'), array('_', '.', ''), $title.'.jpg');
 $result = download($url_photo_big,$img_dir.$tmp_filename);
+$file_name_with_full_path = realpath($img_dir.$tmp_filename);
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 //Uploading the picture and datas to Koken
 $service_url = $koken_api_url;
 $curl = curl_init($service_url);
-$file_name_with_full_path = realpath($img_dir.$tmp_filename);
+
 $curl_post_data = array(
 	'file'          => new CurlFile($file_name_with_full_path, 'image/jpg'),
     'visibility' 	=> 'public',
@@ -154,9 +142,8 @@ curl_setopt($curl, CURLOPT_SAFE_UPLOAD, false);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_TIMEOUT, 100);
 curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('X-Koken-Token: d6e721dd70adea71317c38c95fbd5c56'));
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('X-Koken-Token: '.$koken_token));
 curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
-
 $curl_response = curl_exec($curl);
 
 if ($curl_response === false) {
@@ -169,7 +156,8 @@ $decoded = json_decode($curl_response);
 if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
     die('Koken error occured: ' . $decoded->response->errormessage);
 }
-
+echo 'Upload koken ok!'; //for log
+echo '\n';               //for log
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // Storing id last uploaded picture
@@ -178,7 +166,80 @@ fwrite($fh,$eyeem_photo);
 fclose($fh);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 ///////////////////////////////////////////////////////////////////////////////////////////////
+//
+//                         FUNCTIONS
+//
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+/*****************************************************************************/
+// Replace tag numbers (string like [a:46548] with the name of it
+function replace_tag_number_by_text($string)
+{
+	global $eyeem_token,$eyeem_api_url;
+	$corres = array();
+
+	if (preg_match_all('/\[a:\d+\]/', $string, $matches)) {
+		//on va chercher le titre de chaque
+		foreach ($matches[0] as $item) {
+			//on fait une requete eyeem pour aller chercher le tag
+			preg_match('/\d+/',$item,$eyeem_album_id);
+			
+				///////////////////////////////////////////////////////////////////////////////////////////////
+				//Requesting album details
+
+				$eyeem_album_data = 'albums/'.$eyeem_album_id[0].'?access_token='.$eyeem_token;
+				$album_data = request($eyeem_api_url.$eyeem_album_data);
+
+		// on aggregue les données dans un tableau
+		array_push($corres, array ($item, $album_data->album->name));
+		}
+	}
+	//on fait la substitution : 
+	for ($i = 0; $i <= sizeof($corres)-1; $i++) {	
+		$string = preg_replace($corres[$i][0],$corres[$i][1],preg_replace('/[\[\]]/','',preg_replace('/[\[\]]/','',$string)));
+		}
+	return $string;
+}
+
+
+
+
+/*****************************************************************************/
+// do a curl request
+function request($url)
+{
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$curl_response = curl_exec($curl);
+
+	if ($curl_response === false) {
+		$info = curl_getinfo($curl);
+		curl_close($curl);
+		die('error occured during Eyeem curl exec. Additioanl info: ' . var_export($info));
+	}
+	curl_close($curl);
+	$decoded = json_decode($curl_response);
+	if (isset($decoded->response->status) && $decoded->response->status == 'ERROR') {
+		die('Eyeem error occured: ' . $decoded->response->errormessage);
+	}	
+	return $decoded;
+}
+
+/*****************************************************************************/
+// just download a file
 function download($file_source, $file_target) {
     $rh = fopen($file_source, 'rb');
     $wh = fopen($file_target, 'w+b');
@@ -196,7 +257,6 @@ function download($file_source, $file_target) {
 
     fclose($rh);
     fclose($wh);
-
     return true;
 }
 
